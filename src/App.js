d3848ebd6f2c92a3dd65fcfc89896030fbff1eb5
Eric Paul A. Jaucian <JaucianE@tgtmgmt.local>
2019-07-16 10:14:27 +0800

    Modal Working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ReactModal from "react-modal-resizable-draggable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.openModal}>Open modal</button>
+        <ReactModal
+          initWidth={800}
+          initHeight={400}
+          onRequestClose={this.closeModal}
+          isOpen={this.state.modalIsOpen}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <h3>My Modal</h3>
+          <div className="body">
+            <p>This is the modal&apos;s body.</p>
+          </div>
+          <button onClick={this.closeModal}>Close modal</button>
+        </ReactModal>
+      </div>
+    );
+  }
 }
 
 export default App;
